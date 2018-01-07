e3143c990e31f3173b22ce793b84221242d92f11
rafael <rafael@rafael-Lenovo-G40-70>
2018-01-06 23:04:00 -0200

    Timestamp , trabalhando com hora Mensagens e validação com JSF Colocando o Timezone do sistema como default no xml

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -50,12 +54,20 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("O livro precisa de um autor, "
+					+ "a não ser que foi escrito pelo gasparzinho"));
+			return;
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
 		
 		this.livro = new Livro();
+	}
+	
+	public void comecaComDigitoUm(FacesContext facesContext,UIComponent uiComponent, Object valor) {
+		if(!valor.toString().startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("O ISBN só pode começar com 1, vc (Faustão: -EROUUUU"));
+		}
 	}
 
 }
