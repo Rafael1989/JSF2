b3e648301156f1d1d795a642f2f61d5a7eac974c
rafael <rafael@rafael-Lenovo-G40-70>
2018-01-07 23:29:56 -0200

    Autenticação com phaselistener

--- livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -19,6 +20,7 @@ public class LoginBean {
 	public String efetuaLogin() {
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if(existe) {
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", usuario);
 			return "livro?faces-redirect=true";
 		}
 		return null;
